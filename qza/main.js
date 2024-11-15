ad720dd8553195e25fb1f13139a96e64e60bcf3f
SaadShaikh069 <106235263+SaadShaikh069@users.noreply.github.com>
2024-11-15 12:00:51 +0530

    Add files via upload

--- qza/main.js
+++ qza/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('service-worker.js').then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
+        });
+    }
+
     const trackedData = [];
 
     // Populate trackedData with September data (as before)
