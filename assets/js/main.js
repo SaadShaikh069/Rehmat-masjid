a4c4f70223a4469adacf09981483b57de218c5b2
SaadShaikh069 <saadshaikh069@gmail.com>
2025-08-26 20:17:36 +0530

    indexing issue and code improvement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,15 @@
 $(document).ready(function () {
     var currentPage = window.location.pathname.split('/').pop();
 
-    // Iterate over each <a> tag in the sidebar
+    // Sidebar active link
     $('aside ul li a').each(function () {
         var href = $(this).attr('href');
-
-        // Check if the href matches the current page
         if (href === currentPage) {
-            // Change the color of the corresponding <li>
             $(this).closest('li').addClass('active-link');
         }
     });
-    //sidebar toggle code 
-    // $('#toggleSidebar').on('click', function () {
-    //     $('aside').animate({
-    //         left: $('aside').css('left') == '0px' ? '-100vw' : '0px'
-    //     });
-    // });
-    // $('#closeSidebar').on('click',function(){
-    //     $('aside').css({"left":"-100vw"});
-    // })
 
-
+    // Sidebar toggle
     function toggleSidebar(isOpen) {
         $('aside').animate({ left: isOpen ? '0px' : '-100vw' }, 300, function () {
             $('#overlay').fadeToggle(isOpen);
@@ -38,18 +26,18 @@ $(document).ready(function () {
         toggleSidebar(false);
     });
 
-    // Append rows to the table with indexing and student data
-    students.forEach((val, index) => {
-        $('#example').append('<tr><td>' + (index + 1) + '</td><td>' + val.name + '</td><td>' + val.building + '</td></tr>');
+    // Append student data (without indexing)
+    students.forEach((val) => {
+        $('#example tbody').append('<tr><td></td><td>' + val.name + '</td><td>' + val.building + '</td></tr>');
     });
 
-    // Initialize DataTable with indexing and other configurations
+    // Initialize DataTable
     var table = $('#example').DataTable({
-        "dom": 'Btp', // Include Buttons in the DataTable
-        "pageLength": 50, // Default page length can be set here if needed
-        "ordering": false,
-        "columnDefs": [
-            { "orderable": false, "targets": 0 } // Disable ordering on the indexing column
+        dom: 'Btp',
+        pageLength: 50,
+        ordering: false,
+        columnDefs: [
+            { orderable: false, targets: 0 } // disable sort on Sr#
         ],
         buttons: [
             {
@@ -61,44 +49,47 @@ $(document).ready(function () {
                 extend: 'pdf',
                 text: '<i class="fas fa-file-pdf"></i> PDF',
                 className: 'btn btn-danger'
-            }, {
+            },
+            {
                 text: '<i class="fas fa-image"></i> Download as Image',
                 className: 'btn btn-primary mt-3 w-100',
-                action: function (e, dt, node, config) {
+                action: function () {
                     html2canvas(document.getElementById('example')).then(function (canvas) {
-                        var imgData = canvas.toDataURL('image/png'); // Convert canvas to PNG image
-                        var link = document.createElement('a'); // Create a download link
-                        link.href = imgData; // Set link URL to image data
-                        link.download = 'table_image.png'; // Set the default download filename
-                        link.click(); // Trigger the download
+                        var imgData = canvas.toDataURL('image/png');
+                        var link = document.createElement('a');
+                        link.href = imgData;
+                        link.download = 'table_image.png';
+                        link.click();
                     });
                 }
             }
-        ]
-    });
-
-    // Populate the building filter dropdown with unique building names from the students data
-    var uniqueBuildings = [];
-    students.forEach((val) => {
-        var building = val.building;
-        if (!uniqueBuildings.includes(building)) {
-            uniqueBuildings.push(building);
-            $('#buildingFilter').append('<option value="' + building + '">' + building + '</option>');
+        ],
+        drawCallback: function (settings) {
+            var api = this.api();
+            var startIndex = api.page.info().start;
+            api.column(0, { page: 'current' }).nodes().each(function (cell, i) {
+                cell.innerHTML = startIndex + i + 1;
+            });
         }
     });
 
-    // Filter the table based on the selected building
+    // Populate building filter
+    var uniqueBuildings = [];
+    students.forEach((val) => {
+        if (!uniqueBuildings.includes(val.building)) {
+            uniqueBuildings.push(val.building);
+            $('#buildingFilter').append('<option value="' + val.building + '">' + val.building + '</option>');
+        }
+    });
+
+    // Filter by building
     $('#buildingFilter').on('change', function () {
         var selectedBuilding = $(this).val();
         if (selectedBuilding) {
-            // Escape any special characters in the building name for the regex
-            var escapedBuilding = selectedBuilding.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-            table.column(2).search('^' + escapedBuilding + '$', true, false).draw(); // Adjust index for building column
+            var escaped = selectedBuilding.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            table.column(2).search('^' + escaped + '$', true, false).draw();
         } else {
-            table.column(2).search('').draw(); // Adjust index for building column
+            table.column(2).search('').draw();
         }
     });
-
-
-
 });
